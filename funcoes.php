f7f8a15f19057a7253e063d3b4bf7f692e7aff69
Andrey da Hora Pirola <adahorapirola@gmail.com>
2022-11-09 17:21:41 -0300

    Nada a declarar

--- funcoes.php
+++ funcoes.php
@@ -4,5 +4,17 @@ function geraCoeficiente(float $taxa, int $periodo): float
     return pow( (1+($taxa/100)),$periodo )/ $periodo;
 }
 
-echo geraCoeficienteParcela(2.0, 6);
+function validaMatricula(DateTime $data)
+{
+    $diff = $data->diff(new DateTime());
+    print_r($diff);
+    $anos = $diff->format('%y');
+    if ($anos < 6) {
+        return 'O(a) garoto(a) não pode ser matriculado(a)';
+    } else{
+        return 'Pode matricula-lo!';
+    }
+}
+
+validaMatricula((new DateTime('2020-6-23')));
 ?>
