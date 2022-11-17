c797011f5ea3162c00a31bc66a5bc2625e6eccbd
Andrey da Hora Pirola <adahorapirola@gmail.com>
2022-11-17 17:27:22 -0300

    Criação de tabelas (formularios)

--- cliente.php
+++ cliente.php
@@ -1,7 +1,7 @@
 <?php 
 include 'conecta.php';
 //criando a consulta SQL
-$consultaCliSql = "SELECT * FROM cliente";
+$consultaCliSql = "SELECT * FROM cliente ORDER BY nome ASC, cod_cliente ASC";
 
 //buscando e listando os dados da tabela (conpleta)
 $lista = $conn->query($consultaCliSql);
@@ -9,9 +9,18 @@ $lista = $conn->query($consultaCliSql);
 //separar em linhas
 $row = $lista->fetch();
 print_r($row);
-
+    
 //retornando o númaru de linhas
 $num_rows = $lista->rowCount();
+
+if(isset($_POST['bt-enviar']))
+{
+    $nome = $_POST['nome'];
+    $cpf = $_POST['cpf'];
+    $insertSql = "insert cliente (nome, cpf) values('$nome','$cpf')";
+    $resultado = $conn->query($insertSql);
+    header('location: cliente.php');
+}
 
 ?>
 <!DOCTYPE html>
@@ -20,11 +29,7 @@ $num_rows = $lista->rowCount();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clientes (<?php echo $num_rows?>)</title>
-    <style>
-        td{
-            border-bottom: 1px solid red;
-        }
-    </style>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <table>
@@ -43,5 +48,22 @@ $num_rows = $lista->rowCount();
             <?php } while ($row = $lista->fetch())?>
         </tbody>
     </table>
+        <form action="#" method="post">
+            <div hidden>
+                <label for="cod">Código
+                    <input type="text" name="cod" id=""></label>
+            </div>
+            <div class="campo">
+                <label for="nome">Nome
+                    <input type="text" name="nome" id=""></label>
+            </div>
+            <div class="campo">
+                <label for="cpf">CPF
+                    <input type="number" name="cpf" id=""></label>
+            </div>
+            <div class="campo">
+                <button type="reset" name="bt-enviar">Enviar</button>
+            </div>
+        </form>
 </body>
 </html>
--- funcionario.php
+++ funcionario.php
@@ -5,6 +5,24 @@ $lista = $conn->query($consultaFuncSql);
 $row = $lista->fetch();
 print_r($row);
 $num_rows =$lista->rowCount();
+
+if(isset($_POST['bt-enviar']))
+{
+    $nome = $_POST['nome'];
+    $cpf = $_POST['cpf'];
+    $cargo = $_POST['cargo'];
+    $escala = $_POST['escala'];
+    $turno = $_POST['turno'];
+    $admissao = $_POST['admissao'];
+    $salario = $_POST['salario'];
+    $vt = $_POST['vt'];
+    $vr = $_POST['vr'];
+    $va = $_POST['va'];
+    $insertFuncSql = "insert funcionario (nome, cpf, cargo, escala, turno, admissao, salario, vt, vr, va) 
+        values ('$nome','$cpf','$cargo','$escala','$turno','$admissao','$salario','$vt','$vr','$va')";
+    $resultado = $conn->query($insertFuncSqlt);
+    header('location: funcionario.php');
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,15 +31,10 @@ $num_rows =$lista->rowCount();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Funcionario (<?php echo $num_rows?>)</title>
-    <style>
-        td{
-            border-bottom: 1px solid blue;
-            width: auto;
-        }
-    </style>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <table>
+    <table class="tabelaFunc">
         <thead>
             <th>Cod Funcionário</th>
             <th>Nome</th>
@@ -52,6 +65,74 @@ $num_rows =$lista->rowCount();
             <?php } while ($row = $lista->fetch());?>
         </tbody>
     </table>
-    
 </body>
+        <form action="#" method="post">
+        <div hidden>
+            <label for="cod">Código
+                <input type="text" name="cod" id="">
+            </label>
+        </div>
+        <div>
+            <label for="nome">Nome
+                <input type="text" name="nome" id="">
+            </label>
+        </div>
+        <div>
+            <label for="cpf">CPF
+                <input type="number" name="cpf" id="">
+            </label>
+        </div>
+        <div>
+            <label for="cargo">Cargo
+                <select name="cargo" id="">
+                    <option value="gerente">Gerente</option>
+                    <option value="rh">RH</option>
+                    <option value="adm">ADM</option>
+                    <option value="ti">TI</option>
+                </select>
+            </label>
+        </div>
+        <div>
+            <label for="escala">Escala
+                <input type="datetime" name="escala" id="">
+            </label>
+        </div>
+        <div>
+            <label for="turno">Turno
+                <select name="turno" id="">
+                    <option value="manha">Matutino</option>
+                    <option value="tarde">Tarde</option>
+                    <option value="noite">Noturno</option>
+                </select>
+            </label>
+        </div>
+        <div>
+            <label for="admissao">Admissão
+                <input type="date" name="admissao" id="">
+            </label>
+        </div>
+        <div>
+            <label for="salario">Salário
+                <input type="number" name="salario" id="">
+            </label>
+        </div>
+        <div>
+            <label for="vt">Vale Transporte
+                <input type="number" name="vt" id="">
+            </label>
+        </div>
+        <div>
+            <label for="vr">Vale Refeição
+                <input type="number" name="vr" id="">
+            </label>
+        </div>
+        <div>
+            <label for="va">Vale Alimentação
+                <input type="number" name="va" id="">
+            </label>
+        </div>
+        <div>
+            <button type="reset" name="bt-enviar">Enviar</button>
+        </div>
+    </form>
 </html>
